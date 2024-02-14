33d0cab0b35562bc362247a811d4e3c42571115f
Muthupandi <p.muthupandi@tftus.com>
2024-02-14 22:58:43 +0530

    tried with handlebars template engine

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require('express-handlebars');
+
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs.engine({ extname: '.hbs', defaultLayout: "404"}));
+
+app.set('view engine', 'hbs');
 app.set('views', 'views')
 
 const adminData = require("./routes/admin");
@@ -19,7 +23,8 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404')
 });
 
 app.listen(4000);
