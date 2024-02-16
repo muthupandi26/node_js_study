6ffe70234a6ae249ab6911a55a95580d5e12179a
Muthupandi <p.muthupandi@tftus.com>
2024-02-16 10:24:48 +0530

    updated product model

--- controllers/products.js
+++ controllers/products.js
@@ -2,22 +2,24 @@ const path = require("path");
 
 const rootDir = require("../utils/path");
 
-const Product  = require("../models/product")
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.sendFile(path.join(rootDir, "views", "add-product.html"));
-}
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+};
 
 exports.postAddProduct = (req, res, next) => {
-    console.log(req.body.title);
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect("/");
-}
+  console.log(req.body.title);
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
 
-exports.getProducts =  (req, res, next) => {
-    const products = Product.fetchAll()
-    // res.sendFile(path.join(rootDir, "views", "shop.html"));
-    res.render('shop', { prods: products, docTitle: "dummy shop" });
-}
-
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    console.log(products);
+    // in a render i didn't map just priniting 1st one value
+    res.render("shop", { prods: products, docTitle: "dummy shop" });
+  });
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+};
